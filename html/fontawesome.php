f90d7392f3d85fdd42d312b3c646fbf0a4437565
KerlyJ <kerly.jimenez@espoch.edu.ec>
2020-01-18 15:01:58 -0500

    cambio a php

--- html/fontawesome.php
+++ html/fontawesome.php
@@ -47,7 +47,7 @@
             <div class="navbar-header">
                 <div class="top-left-part">
                     <!-- Logo -->
-                    <a class="logo" href="dashboard.html">
+                    <a class="logo" href="dashboard.php">
                         <!-- Logo icon image, you can use font-icon also --><b>
                             <!--This is dark logo icon--><img src="../plugins/images/admin-logo.png" alt="home"
                                 class="dark-logo" />
@@ -94,31 +94,31 @@
                 </div>
                 <ul class="nav" id="side-menu">
                     <li style="padding: 70px 0 0;">
-                        <a href="dashboard.html" class="waves-effect"><i class="fa fa-clock-o fa-fw"
+                        <a href="dashboard.php" class="waves-effect"><i class="fa fa-clock-o fa-fw"
                                 aria-hidden="true"></i>Dashboard</a>
                     </li>
                     <li>
-                        <a href="profile.html" class="waves-effect"><i class="fa fa-user fa-fw"
+                        <a href="profile.php" class="waves-effect"><i class="fa fa-user fa-fw"
                                 aria-hidden="true"></i>Profile</a>
                     </li>
                     <li>
-                        <a href="basic-table.html" class="waves-effect"><i class="fa fa-table fa-fw"
+                        <a href="basic-table.php" class="waves-effect"><i class="fa fa-table fa-fw"
                                 aria-hidden="true"></i>Basic Table</a>
                     </li>
                     <li>
-                        <a href="fontawesome.html" class="waves-effect"><i class="fa fa-font fa-fw"
+                        <a href="fontawesome.php" class="waves-effect"><i class="fa fa-font fa-fw"
                                 aria-hidden="true"></i>Icons</a>
                     </li>
                     <li>
-                        <a href="map-google.html" class="waves-effect"><i class="fa fa-globe fa-fw"
+                        <a href="map-google.php" class="waves-effect"><i class="fa fa-globe fa-fw"
                                 aria-hidden="true"></i>Google Map</a>
                     </li>
                     <li>
-                        <a href="blank.html" class="waves-effect"><i class="fa fa-columns fa-fw"
+                        <a href="blank.php" class="waves-effect"><i class="fa fa-columns fa-fw"
                                 aria-hidden="true"></i>Blank Page</a>
                     </li>
                     <li>
-                        <a href="404.html" class="waves-effect"><i class="fa fa-info-circle fa-fw"
+                        <a href="404.php" class="waves-effect"><i class="fa fa-info-circle fa-fw"
                                 aria-hidden="true"></i>Error 404</a>
                     </li>
                 </ul>
--- html/tablas.php
+++ html/tablas.php
@@ -47,7 +47,7 @@
             <div class="navbar-header">
                 <div class="top-left-part">
                     <!-- Logo -->
-                    <a class="logo" href="dashboard.html">
+                    <a class="logo" href="dashboard.php">
                         <!-- Logo icon image, you can use font-icon also --><b>
                             <!--This is dark logo icon--><img src="../plugins/images/logoAdmin2.png" alt="home"
                                 class="dark-logo" />
