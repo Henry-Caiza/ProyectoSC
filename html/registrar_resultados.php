71a792f5c3100e3344884d4dd81050a531b2b3b2
AdriTris <barreraadriana33@gmail.com>
2020-01-29 15:33:08 -0500

    resultados

--- html/registrar_resultados.php
+++ html/registrar_resultados.php
@@ -195,7 +195,7 @@
 				  <td><?php echo $filas['goles_equipo2'] ?></td>
                   <td><?php echo $filas['tarj_ama_eq2'] ?></td>
                   <td><?php echo $filas['tarj_roj_eq2'] ?></td>
-				  <td><a href='CRUD/update_resultados.php?id= <?php echo $filas['id']; ?>'  class="btn__update" >Editar</a></td>
+				  <td><a href='CRUD/update_resultados.php?id= <?php echo $filas['id']; ?>'  class="btn__update" >Ingresar/Editar</a></td>
 				  <td><a href='CRUD/delete_jugadores.php?id= <?php echo $filas['id']; ?>' class="btn__delete" onclick=" return preguntar()">Eliminar</a></td>
                 </tr>
 				<?php } ?>
